fde0ebda52cd5511793eafceaa24f3fe430d7ef2
Arshia Zakeri Rad <zrr.arshia@gmail.com>
2023-07-14 13:41:10 -0400

    changed api paths

--- cms_back/routes/index.js
+++ cms_back/routes/index.js
@@ -2,12 +2,12 @@ const path = require("path");
 const router = require("express").Router();
 
 // Test Api
-router.get("/api/v1/status", async (req, res, next) => {
+router.get("/status", async (req, res, next) => {
   return res.status(200).json({ success: true, msg: "API is up" });
 });
 
 router.use("/images", require(path.join(__dirname, "./images")));
-router.use("/api/v1/management", require(path.join(__dirname, "./management")));
-router.use("/api/v1/contacts", require(path.join(__dirname, "./contacts")));
+router.use("/contacts", require(path.join(__dirname, "./contacts")));
+router.use("/panel", require(path.join(__dirname, "./panel")));
 
 module.exports = router;
