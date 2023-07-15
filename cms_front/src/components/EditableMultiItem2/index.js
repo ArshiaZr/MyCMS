e545974d8f6302eabbe0adec46a4007cf2d658c1
Arshia Zakeri Rad <zrr.arshia@gmail.com>
2023-07-14 20:53:40 -0400

    updated

--- cms_front/src/components/EditableMultiItem2/index.js
+++ cms_front/src/components/EditableMultiItem2/index.js
@@ -5,6 +5,8 @@ import { FiDelete } from "react-icons/fi";
 import { useEffect, useRef } from "react";
 import Select from "../Select";
 import EditableTextInput from "../EditableTextInput";
+import TextArea from "../TextArea";
+import { interprets } from "../../../constants";
 
 export default function EditableMultiItem2({
   originalItems = [],
@@ -47,13 +49,14 @@ export default function EditableMultiItem2({
                 onChange={(e) => onChange(idx, "title", e.target.value, idxRow)}
                 border={true}
               />
-              <EditableTextInput
-                border={true}
+              <TextArea
                 name="detail"
                 originalValue={item.detail ? item.detail : "Detail"}
-                onChange={(e) =>
-                  onChange(idx, "detail", e.target.value, idxRow)
-                }
+                onChange={(e) => {
+                  onChange(idx, "detail", e.target.value, idxRow);
+                }}
+                interprets={interprets}
+                border={true}
               />
               <Select
                 options={Array.apply(null, Array(originalItems.length)).map(
